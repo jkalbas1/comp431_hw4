cf885d2b4326599aa1c9d06ea4214cf46dc0b7b4
jkalbas <joeykalbas@gmail.com>
2023-02-20 16:49:34 -0500

    chenged

--- Server.py
+++ Server.py
@@ -329,7 +329,23 @@ while(True):
             exit(1)
         if(recv_msg[0:4] == "QUIT"):
             break
-        
+
+        if(recv_msg[0] == " " or recv_msg[0] == "\t"):
+            recv_msg = recv_msg.strip()
+            if not check_valid_cmd(recv_msg):
+                send_msg = "500 Syntax error: command unrecognized"
+            else:
+                send_msg = "501 Syntax error in parameters or arguments"
+            try:
+                connSock.send(send_msg.encode())
+            except socket.error as e:
+                print("Send error")
+                connSock.close()
+                state = ""
+                receivers = []
+                data_seen = ""
+                continue
+
         if not check_valid_cmd(recv_msg) and state != "DATA":
             send_msg = "500 Syntax error: command unrecognized"
             try:
