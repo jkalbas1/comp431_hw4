5602dff61ff23951e785b99b4d3960ab8b306424
jkalbas <joeykalbas@gmail.com>
2023-02-21 11:21:38 -0500

    ok:

--- Server.py
+++ Server.py
@@ -278,16 +278,18 @@ sender = ""
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 hostname = socket.gethostname()
 port = int(argv[1])
-try:
-    serverSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-except socket.error as e:
-    print("Socket sockopt failure.")
-    serverSock.close()
-    exit(1)
+
 try:
     serverSock.bind(('', port))
 except socket.error as e:
     print("Socket bind failure.")
+    serverSock.close()
+    exit(1)
+
+try:
+    serverSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+except socket.error as e:
+    print("Socket sockopt failure.")
     serverSock.close()
     exit(1)
 
